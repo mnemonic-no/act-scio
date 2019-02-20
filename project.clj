507ce59baf9cc91bf66577c7a4f67ea3823786ba
dev-239 <dev239@protonmail.com>
2019-02-20 13:39:21 +0100

    Fixed project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject scio-back "0.1.39-SNAPSHOT"
-  :description "Storing tweets and documents to alastic search for indexing."
+  :description "Storing tweets and documents to Elasticsearch for indexing."
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.apache.commons/commons-compress "1.13"] ;; "fix" for missing class in pantomime
                  [com.novemberain/pantomime "2.9.0"]
