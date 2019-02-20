88fa7a4557c8a248f0d66623c913475451882d74
dev-239 <dev239@protonmail.com>
2019-02-20 12:13:41 +0100

    Reworked geonames_download.sh to handle missing files and dirs

--- etc/geonames_download.sh
+++ etc/geonames_download.sh
@@ -1,14 +1,69 @@
 #!/usr/bin/env sh
-mkdir download
-mkdir bak
-mv cities1000.txt bak/.
-curl http://download.geonames.org/export/dump/cities1000.zip -o download/cities1000.zip
-unzip download/cities1000.zip
-mv cities15000.txt bak/.
-curl http://download.geonames.org/export/dump/cities15000.zip -o download/cities15000.zip
-unzip download/cities15000.zip
-mv countryInfo.txt bak/.
-curl http://download.geonames.org/export/dump/countryInfo.txt -o countryInfo.txt
-sed -i '/^#/d' countryInfo.txt
-mv ISO-3166-countries-with-regional-codes.json bak/
-curl https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json -o ISO-3166-countries-with-regional-codes.json
+set -e
+
+DOWNLOAD_DIR="download"
+BACKUP_DIR="bak"
+COUNTRY_INFO="countryInfo.txt"
+CITIES_1000="cities1000"
+CITIES_15000="cities15000"
+GEONAMES_URL="http://download.geonames.org/export/dump"
+COUNTRIES_WITH_REGION_CODES_URL="https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json"
+COUNTRIES_WITH_REGION_CODES_JSON="ISO-3166-countries-with-regional-codes.json"
+
+function log {
+    echo `date "+%F %T"` $1
+}
+
+function create_if_not_exists {
+    if [ ! -d "$1" ]; then
+        log "creating folder $1"
+        mkdir $1
+    fi
+}
+
+function backup_if_exists {
+    if [ -f "$1" ]; then
+        log "backing up $1"
+        mv $1 $BACKUP_DIR/.
+    fi
+}
+
+function download_from_geonames {
+    curl --silent $GEONAMES_URL/$1 -o $DOWNLOAD_DIR/$1
+}
+
+function download_cities {
+    backup_if_exists "$1.txt"
+    download_from_geonames "$1.zip"
+    unzip -q $DOWNLOAD_DIR/$1.zip
+    rm $DOWNLOAD_DIR/$1.zip
+}
+
+function download_country_info {
+    backup_if_exists $1
+    download_from_geonames $1
+    sed -i '' '/^#/d' $DOWNLOAD_DIR/$1
+    mv $DOWNLOAD_DIR/$1 .
+}
+
+log "checking if all folders exist"
+create_if_not_exists $DOWNLOAD_DIR
+create_if_not_exists $BACKUP_DIR
+
+log "downloading files"
+
+log "getting $CITIES_1000"
+download_cities $CITIES_1000
+
+log "getting $CITIES_15000"
+download_cities $CITIES_15000
+
+log "getting $COUNTRY_INFO"
+download_country_info $COUNTRY_INFO
+
+log "getting $COUNTRIES_WITH_REGION_CODES"
+backup_if_exists $COUNTRIES_WITH_REGION_CODES
+curl --silent $COUNTRIES_WITH_REGION_CODES_URL -o $COUNTRIES_WITH_REGION_CODES_JSON
+
+log "removing $DOWNLOAD_DIR"
+rm -r $DOWNLOAD_DIR
