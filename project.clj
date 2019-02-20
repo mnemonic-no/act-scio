632266aba7289397cb7c3fbb364793a2e3fbdc33
dev-239 <dev239@protonmail.com>
2019-02-20 13:46:13 +0100

    Updated dependecies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject scio-back "0.1.39-SNAPSHOT"
   :description "Storing tweets and documents to Elasticsearch for indexing."
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.apache.commons/commons-compress "1.13"] ;; "fix" for missing class in pantomime
-                 [com.novemberain/pantomime "2.9.0"]
+                 [org.apache.commons/commons-compress "1.18"] ;; "fix" for missing class in pantomime
+                 [com.novemberain/pantomime "2.10.0"]
                  [info.debatty/java-spamsum "0.2"]
-                 [clj-http "3.7.0"]
-                 [org.clojure/algo.generic "0.1.2"]
+                 [clj-http "3.9.1"]
+                 [org.clojure/algo.generic "0.1.3"]
                  [clojure-ini "0.0.2"]
-                 [clojure-opennlp "0.4.0"]
-                 [digest "1.4.6"]
+                 [clojure-opennlp "0.5.0"]
+                 [digest "1.4.8"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/core.async "0.3.443"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [beanstalk-clj "0.1.3"]]
   :main ^:skip-aot scio-back.core
