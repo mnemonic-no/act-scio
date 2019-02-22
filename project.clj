2851d4c7a8f05f51b5360e198d6d1f5e86b1f03f
dev-239 <dev239@protonmail.com>
2019-02-22 21:50:54 +0100

    Added org.clojure/tools.cli, removed unused dep, fixed typo

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject scio-back "0.1.39-SNAPSHOT"
-  :description "Storing tweets and documents to alastic search for indexing."
+  :description "Storing tweets and documents to Elasticsearch for indexing."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.commons/commons-compress "1.13"] ;; "fix" for missing class in pantomime
-;;                 [org.apache.tika/tika-parsers "1.16"]
                  [com.novemberain/pantomime "2.9.0"]
                  [info.debatty/java-spamsum "0.2"]
                  [clj-http "3.7.0"]
@@ -15,7 +14,8 @@
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/tools.logging "0.4.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [beanstalk-clj "0.1.3"]]
+                 [beanstalk-clj "0.1.3"]
+                 [org.clojure/tools.cli "0.4.1"]]
   :main ^:skip-aot scio-back.core
   :target-path "target/%s"
   :keep-non-project-classes true
